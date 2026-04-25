e81ab98dc6d805715e77d75b721926d31a49a623
MishaLehotskyi <eglet777@ukr.net>
2026-04-25 17:47:16 +0200

    feat: added jpa + liquidbase + soft delete

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,13 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Book;
-import java.util.List;
-import java.util.Optional;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-
-    Optional<Book> findById(Long id);
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE Book b SET b.isDeleted = true WHERE b.id = :id")
+    void deleteById(@NonNull Long id);
 }
--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.dto.BookDto;
 import com.example.demo.dto.CreateBookRequestDto;
+import com.example.demo.dto.UpdateBookRequestDto;
 import com.example.demo.exception.EntityNotFoundException;
 import com.example.demo.mapper.BookMapper;
+import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -32,5 +34,18 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookDto save(CreateBookRequestDto bookDto) {
         return bookMapper.toDto(bookRepository.save(bookMapper.toModel(bookDto)));
+    }
+
+    @Override
+    public BookDto update(Long id, UpdateBookRequestDto bookDto) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id: " + id));
+        bookMapper.updateBookFromDto(bookDto, book);
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
     }
 }
